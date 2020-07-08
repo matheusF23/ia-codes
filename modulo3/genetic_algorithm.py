2b12b04d5f02f6af04deae104aaff0f02a763cfd
Matheus Figueiredo <matheus_figueiredo18@hotmail.com>
2020-07-07 23:02:17 -0300

    adição de representaçao de float

--- modulo3/genetic_algorithm.py
+++ modulo3/genetic_algorithm.py
@@ -10,7 +10,7 @@ import math
 from matplotlib import pyplot as plt
 
 
-def convert_to_decimal(chromosome):
+def convert_to_float(chromosome):
     decimal = 0
     chromosome = chromosome[::-1]
     length = len(chromosome)
@@ -18,12 +18,14 @@ def convert_to_decimal(chromosome):
         if chromosome[i] == 1:
             decimal = decimal + 2**i
 
-    return decimal
+    float_point = -3 + 6*(decimal/((2**8) - 1))
+
+    return float_point
 
 
 def fitness_function(x):
-    # return 15*x - x**2
-    return -20*math.exp(-0.2*math.sqrt((1/2)*x**2))-math.exp((1/2)*(math.cos(2*math.pi*x)))+20+math.exp(1)
+    return 2*x*math.cos(x**2)
+    # return -20*math.exp(-0.2*math.sqrt((1/2)*x**2))-math.exp((1/2)*(math.cos(2*math.pi*x)))+20+math.exp(1)
 
 
 def create_population(population_size):
@@ -32,12 +34,10 @@ def create_population(population_size):
 
     # Criando população inicial
     while(len(population) != population_size):
-        for i in range(5):
+        for i in range(8):
             chromosome.append(random.randint(0, 1))
 
-        if(chromosome not in population):
-            population.append(chromosome)
-
+        population.append(chromosome)
         chromosome = []
 
     return population
@@ -68,24 +68,27 @@ def mutation(children, mutation_rate):
             child[position] = 0**child[position]
 
 
-def genetic_algorithm(population_size, crossing_rate=0.7, mutation_rate=0.01, generations=100):
+def genetic_algorithm(population_size, crossing_rate=0.8, mutation_rate=0.001, generations=100):
     generation = 0
     population = create_population(population_size)
     mean_list = []
-    generation_list = [generation]
+    max_list = []
+    generation_list = []
     while(generation < generations):
-        fitness = [fitness_function(convert_to_decimal(chrom))
+        fitness = [fitness_function(convert_to_float(chrom))
                    for chrom in population]
 
-        mean = int(sum(fitness) / len(fitness))
+        # Seleciona o melhor cromossomo para passar para a próxima geração
+        best_chromosome = population[fitness.index(max(fitness))]
+
+        mean = sum(fitness) / len(fitness)
+        generation_list.append(generation)
         mean_list.append(mean)
+        max_list.append(max(fitness))
+
         error = max(fitness) - mean
-        if(error < 2):
-            for i, v in enumerate(fitness):
-                if (v == max(fitness)):
-                    plt.plot(generation_list, mean_list)
-                    plt.show()
-                    return convert_to_decimal(population[i]), generation
+        if(error < 0.1):
+            break
 
         population_with_weight = [(population[i], int(
             (fitness[i] / sum(fitness)) * 100) + 1) for i in range(len(population))]
@@ -103,16 +106,33 @@ def genetic_algorithm(population_size, crossing_rate=0.7, mutation_rate=0.01, ge
             parents.append((father, mother))
 
         children = crossing_over(parents, crossing_rate)
+        # Adicionar o melhor cromossomo no lugar do pior
+        fitness_child = [fitness_function(convert_to_float(chrom))
+                         for chrom in children]
+        children[fitness_child.index(min(fitness_child))] = best_chromosome
+
         mutation(children, mutation_rate)
         population = children[:]
         generation += 1
-        generation_list.append(generation)
 
     for i, v in enumerate(fitness):
         if (v == max(fitness)):
-            plt.plot(generation_list, mean_list)
+            plt.figure(num="Aptidão média (azul) e melhor aptidão (laranja) da população")
+            plt.plot(generation_list, mean_list, generation_list, max_list)
+            plt.title('Aptidão média e melhor aptidão da população')
+            plt.ylabel('Aptidão')
+            plt.xlabel('Geração')
+
+            # plt.figure(num="Melhor aptidão da população")
+            # plt.plot(generation_list, max_list)
+            # plt.title(f'Geração {generation}')
+            # plt.ylabel('Melhor aptidão da população')
+            # plt.xlabel('Geração')
+
             plt.show()
-            return convert_to_decimal(population[i]), generation
+            return (f'Melhor valor de x: {convert_to_float(population[i])}',
+                    f'f(x) = {v}',
+                    f'Quantidade de gerações: {generation}')
 
 
 # crossing_rate = float(input("Informe a taxa de cruzamento (entre 0 e 1): "))
@@ -121,4 +141,6 @@ population_size = int(input("Informe o tamanho da população: "))
 # # generations = int(input("Informe a quantidade de gerações: "))
 
 # , crossing_rate, mutation_rate, generations))
-print(genetic_algorithm(population_size))
+print()
+for i in genetic_algorithm(population_size):
+    print(i)
